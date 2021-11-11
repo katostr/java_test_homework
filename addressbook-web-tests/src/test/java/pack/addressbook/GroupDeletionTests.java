1d551b1bacd7987f6472d30166f662d3b11667ab
katostr <katostrov1995@gmail.com>
2021-11-12 01:51:58 +0300

    Созданы классы TestBase и GroupDeletionTests
    Задание 5

---
+++ addressbook-web-tests/src/test/java/pack/addressbook/GroupDeletionTests.java
@@ -0,0 +1,17 @@
+package pack.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroup();
+        returnToGroupPage();
+    }
+
+}
+
+
